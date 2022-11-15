f3085b36ac588ffefa946e41d11f737f99db42e0
1325612 <50739388+1325612@users.noreply.github.com>
2022-11-15 12:06:45 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -22,8 +22,9 @@ const USER_CONFIG = {
       name: '宝贝',
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
       id: 'o-l3t6ucC1Xva_O5PqMxCGb7BACw',
+      id: 'o-l3t6jwoZO0T59V7cjNWF6FGTcE',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
-      useTemplateId: 'MnDvsAjFpRz4Jmxgs1j8X7araDDbx2gXtCdN2Jj0hOE',
+      useTemplateId: '1m-sQMrS6YtyZ5r0Zs-vcRIL_SVqs7PU-q-aelxHOsU',
       // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
       horoscopeDate: '03-25',
       festivals: [
@@ -56,6 +57,9 @@ const USER_CONFIG = {
       name: '自己',
       // 使用微信测试号：自己的微信id，扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
       id: 'o-l3t6ucC1Xva_O5PqMxCGb7BACw',
+      name: '宝宝',
+      // 使用微信测试号：自己的微信id，扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
+      id: 'o-l3t6jwoZO0T59V7cjNWF6FGTcE',
     }
   ],
 
